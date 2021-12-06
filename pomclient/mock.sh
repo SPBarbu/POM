8cc568161972ab20c49e9a94d089f1b8a25a9c31
SPBarbu <stefpb0@live.ca>
2021-12-05 22:50:49 -0500

    mocking

---
+++ pomclient/mock.sh
@@ -0,0 +1,4 @@
+(sleep 5; echo 14) | ./test dev1POM.xml pomserver 8000
+(sleep 15; echo 14) | ./test dev2POM.xml pomserver 8000
+(sleep 25; echo 14) | ./test dev3POM.xml pomserver 8000
+(sleep 35; echo 14) | ./test dev4POM.xml pomserver 8000
